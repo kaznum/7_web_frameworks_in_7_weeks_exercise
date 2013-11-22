9e92d194db92e8b2b85de1b1ade098aec3546341
Kazuya NUMATA <numata@gmail.com>
2013-11-22 16:36:41 +0900

    Implement "Functinal Forms" examples
    Implement postUserR

--- 6_yesod/rumble/v2/rumble/Handler/Users.hs
+++ 6_yesod/rumble/v2/rumble/Handler/Users.hs
@@ -9,7 +9,13 @@ getUsersR = do
   defaultLayout $(widgetFile "users")
 
 postUsersR :: Handler Html
-postUsersR = error "Not yet implemented: postUsersR"
+postUsersR = do
+  ((result, _), _) <- runFormPost userForm
+  case result of
+    FormSuccess user -> do
+                _ <- runDB $ insert user
+                redirect UsersR
+    _ -> defaultLayout [whamlet|whoops|]
 
 userForm :: Form User
 userForm = renderDivs $ User
