10f5146a6229a3f5915ce1d54001a6b5d974fd9b
Kazuya NUMATA <numata@gmail.com>
2013-11-25 17:08:38 +0900

    Implement "Viewing Posts and Making Comments" examples

--- 6_yesod/rumble/v2/rumble/Handler/Comments.hs
+++ 6_yesod/rumble/v2/rumble/Handler/Comments.hs
@@ -1,6 +1,24 @@
 module Handler.Comments where
 
 import Import
+import Data.Time
+import Yesod.Auth
+
+makeComment :: PostId -> Textarea -> UserId -> UTCTime -> Comment
+makeComment post body = \author time -> Comment post author time body
+
+commentForm :: PostId -> Form (UserId -> UTCTime -> Comment)
+commentForm post = renderDivs $ makeComment post
+                   <$> areq textareaField "Comment" Nothing
 
 postCommentsR :: PostId -> Handler Html
-postCommentsR = error "Not yet implemented: postCommentsR"
+postCommentsR post = do
+  authorId <- requireAuthId
+  ((result, _), _) <- runFormPost $ commentForm post
+  case result of
+    FormSuccess mkComment -> do
+      time <- liftIO getCurrentTime
+      _ <- runDB $ insert $ mkComment authorId time
+      redirect $ PostR post
+    _ -> defaultLayout [whamlet|whoops|]
+
--- 6_yesod/rumble/v2/rumble/Handler/Post.hs
+++ 6_yesod/rumble/v2/rumble/Handler/Post.hs
@@ -1,6 +1,23 @@
 module Handler.Post where
 
 import Import
+import Data.Maybe
+import Yesod.Auth
+import Handler.Comments (commentForm)
 
 getPostR :: PostId -> Handler Html
-getPostR = error "Not yet implemented: getPostR"
+getPostR postId = do
+  authId <- maybeAuthId
+  (post, author, comments, commentsWithAuthors) <- runDB $ do
+    post <- get404 postId
+    author <- get404 $ postAuthor post
+    comments <- selectList [CommentPost ==. postId] [Asc CommentCreated]
+    authors <- mapM (get . commentAuthor . entityVal) comments
+    return (post, author, comments, zip (map entityVal comments) (map fromJust authors))
+  (commentFormWidget, enctype) <- generateFormPost $ commentForm postId
+  defaultLayout $ do
+    setTitle $ toHtml $ (postTitle post) <> " - Rumble"
+    $(widgetFile "post-full")
+
+generateCommentWidget :: Comment -> User -> Widget
+generateCommentWidget comment author = $(widgetFile "comment")
