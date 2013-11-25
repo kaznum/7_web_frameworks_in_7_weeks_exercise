3e71c1ff7cdcb8f925a7ee46d2ed6bf0101fd64c
Kazuya NUMATA <numata@gmail.com>
2013-11-25 16:27:58 +0900

    Implement "Building a Post Form" examples

--- 6_yesod/rumble/v2/rumble/Handler/PostNew.hs
+++ 6_yesod/rumble/v2/rumble/Handler/PostNew.hs
@@ -1,9 +1,28 @@
 module Handler.PostNew where
 
 import Import
+import Data.Time
+import Yesod.Auth
+
+postForm :: Form (UserId -> Int -> UTCTime -> Post)
+postForm = renderDivs $ Post
+           <$> areq textField "Title" Nothing
+           <*> areq textField "URL" Nothing
 
 getPostNewR :: Handler Html
-getPostNewR = error "Not yet implemented: getPostNewR"
+getPostNewR = do
+  _ <- requireAuthId
+  (postFormWidget, enctype) <- generateFormPost postForm
+  defaultLayout $(widgetFile "post-new")
 
 postPostNewR :: Handler Html
-postPostNewR = error "Not yet implemented: postPostNewR"
+postPostNewR = do
+  authorId <- requireAuthId
+  ((result, _), _) <- runFormPost postForm
+  case result of
+    FormSuccess makePost -> do
+      time <- liftIO getCurrentTime
+      post <- runDB $ insert $ makePost authorId 0 time
+      redirect $ PostR post
+    _ -> defaultLayout [whamlet|whoops|]
+
