56276b01a7a6681c912705c2916dc0ac452b8f4f
Kazuya NUMATA <numata@gmail.com>
2013-11-28 13:20:02 +0900

    Implement "Rendering Views" examples

---
+++ 2_canjs/public/app/base/app-test.js
@@ -0,0 +1,13 @@
+var bookmarks = [
+  {url: "http://one.com", title:"One"},
+  {url: "http://two.com", title:"Two"}
+];
+
+var viewModel = {bookmarks:bookmarks};
+var element = $("#target");
+
+element.html(can.view("/app/base/bookmark_list", viewModel));
+
+// can.view is implicitly called
+element.html("/app/base/bookmark_list", viewModel);
+
