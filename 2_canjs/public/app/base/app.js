224a1a9778bdc44144bf4759978f21ec5c92603c
Kazuya NUMATA <numata@gmail.com>
2013-11-28 16:12:06 +0900

    Implement "Adding Validation" examples

--- 2_canjs/public/app/base/app.js
+++ 2_canjs/public/app/base/app.js
@@ -7,6 +7,18 @@ var Bookmark = can.Model.extend({
 }, {
 });
 
+var ValidatingBookmark = Bookmark.extend({
+  init: function() {
+    var urlPattern = new RegExp(
+      "(http|https):\\/\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?" +
+        "(\\/|\\/([\\w#!:.?+=&%@!\\-\\/]))?");
+
+    this.validatePresenceOf(["url", "title"]);
+    this.validateFormatOf("url", urlPattern);
+  }
+}, {
+});
+
 var BookmarkListControl = can.Control.extend({
   view: "/app/base/bookmark_list",
 
@@ -67,14 +79,37 @@ var BookmarkFormControl = can.Control.extend({
   }
 });
 
+var ValidatingBookmarkFormControl = BookmarkFormControl.extend({
+  BookmarkModel: ValidatingBookmark,
+
+  editBookmark: function(bookmark) {
+    this._super(bookmark);
+
+    var self = this;
+    bookmark.bind("change", function () {
+      var errorMessage = bookmark.errors() ? can.map(bookmark.errors(),
+                                                     function(message, attrName) {
+                                                       return attrName + " " + message + ". ";
+                                                     }).join("") : "";
+      self.element.find(".text-error").html(errorMessage);
+    });
+  },
+
+  saveBookmark: function (bookmark) {
+    if (!bookmark.errors()) {
+      this._super(bookmark);
+    }
+  }
+});
+
 var App_base = can.Construct.extend({
   init: function() {
-    Bookmark.findAll({}, function(bookmarks) {
+    ValidatingBookmark.findAll({}, function(bookmarks) {
       var eventHub = new can.Observe({});
       var options = {eventHub: eventHub, bookmarks: bookmarks};
 
       new BookmarkListControl("#bookmark_list_container", options);
-      new BookmarkFormControl("#bookmark_form_container", options);
+      new ValidatingBookmarkFormControl("#bookmark_form_container", options);
     });
   }
 });
