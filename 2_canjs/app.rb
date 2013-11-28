67fcf5aae2ba3b08f857d6a912d35a4f7acc3bf8
Kazuya NUMATA <numata@gmail.com>
2013-11-28 15:43:58 +0900

    Implmement "Use an Observe to Communicate Between Controllers", "Creating a Form Control" examples

---
+++ 2_canjs/app.rb
@@ -0,0 +1,123 @@
+require 'sinatra'
+require 'data_mapper'
+require 'dm-serializer'
+require 'sinatra/respond_with'
+require "sinatra/mustache"
+require_relative 'bookmark'
+require_relative 'tagging'
+require_relative 'tag'
+require 'json'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/bookmarks.db")
+DataMapper::finalize.auto_upgrade!
+
+before %r{/bookmarks/(\d+)} do |id|
+  @bookmark = Bookmark.get(id.to_i)
+  if !@bookmark
+    halt 404, "bookmark #{id} not found"
+  end
+end
+
+with_tagList = {methods: [:tagList]}
+
+def get_all_bookmarks
+  Bookmark.all(order: :title)
+end
+
+get "/" do
+  @bookmarks = get_all_bookmarks
+  mustache :index
+end
+
+get "/bookmarks" do
+  content_type :json
+  get_all_bookmarks.to_json with_tagList
+end
+
+
+post "/bookmarks" do
+  input = params.empty? ? JSON.parse(request.body.read) : params
+
+  bookmark = Bookmark.new input.only("url", "title")
+  if bookmark.save
+    add_tags(bookmark, input)
+    [201, bookmark.to_json(with_tagList)]
+  else
+    400 #bad request
+  end
+end
+
+class Hash
+  def only(*whitelist)
+    whitelist.inject({}) { |result, key| result.merge(key => self[key])}
+  end
+end
+
+get %r{/bookmarks/\d+} do
+  content_type :json
+  @bookmark.to_json with_tagList
+end
+
+
+get "/bookmarks/*" do
+  tags = params[:splat].first.split '/'
+  bookmarks = Bookmark.all
+  tags.each do |tag|
+    bookmarks = bookmarks.all({ taggings: { tag: { label: tag }}})
+  end
+  bookmarks.to_json with_tagList
+end
+
+get "/test/:one/:two" do |creature, sound|
+  "a #{creature} says #{sound}"
+end
+
+put %r{/bookmarks/\d+} do
+  input = params.empty? ? JSON.parse(request.body.read) : params
+
+  if @bookmark.update input.only("url", "title")
+    add_tags(@bookmark, input)
+    200 #OK
+  else
+    400 # bad request
+  end
+end
+
+
+delete %r{/bookmarks/\d+} do
+  @bookmark.destroy
+  200
+end
+
+get "/bookmark/new" do
+  slim :bookmark_form_new
+end
+
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+
+  def add_tags(bookmark, input)
+    labels = input["tagList"].is_a?(String) ? input["tagList"].split(',').map(&:strip) : (input["tagList"] || []).map(&:strip)
+
+    existing_labels = []
+    bookmark.taggings.each do |tagging|
+      if labels.include? tagging.tag.label
+        existing_labels.push tagging.tag.label
+      else
+        tagging.destroy
+      end
+    end
+
+    (labels - existing_labels).each do |label|
+      tag = { label: label }
+      existing = Tag.first tag
+      if !existing
+        existing = Tag.create tag
+      end
+      Tagging.create tag: existing, bookmark: bookmark
+    end
+  end
+end
+
--- 2_canjs/public/app/base/app.js
+++ 2_canjs/public/app/base/app.js
@@ -7,17 +7,76 @@ var Bookmark = can.Model.extend({
 }, {
 });
 
+var BookmarkListControl = can.Control.extend({
+  view: "/app/base/bookmark_list",
 
-Bookmark.findAll({}, function(bookmarks) {
+  init: function(element, options) {
+    this.eventHub = options.eventHub;
+    var view = options.view || this.view;
+    element.html(view, this.getViewModel(options));
+  },
+  getViewModel: function (options) {
+    return {bookmarks: options.bookmarks}
+  },
+  getBookmark: function (el) {
+    return el.closest("li").data("bookmark");
+  },
+  ".delete click": function(el, evt) {
+    this.getBookmark(el).destroy();
+  },
+  ".edit click": function(el, evt) {
+    can.trigger(this.eventHub, "editBookmark", this.getBookmark(el));
+  }
 });
 
-Bookmark.findOne({id:42}, function(bookmark) {
+var BookmarkFormControl = can.Control.extend({
+  BookmarkModel: Bookmark,
+  view: "/app/base/bookmark_form",
+  init: function (element, options) {
+    this.BookmarkModel.bind("created", function(evt, bookmark) {
+      options.bookmarks.push(bookmark);
+    });
+    this.clearForm();
+  },
+  editBookmark: function(bookmark) {
+    var view = this.options.view || this.view;
+    this.element.html(view, bookmark);
+    bookmark.bind("destroyed", this.clearForm.bind(this));
+  },
+  clearForm: function () {
+    this.editBookmark(new this.BookmarkModel());
+  },
+  "{eventHub} editBookmark": function(eventHub, evt, bookmark) {
+    this.editBookmark(bookmark);
+  },
+  ".save click": function(el, evt) {
+    evt.preventDefault();
+    var bookmark = el.data("bookmark");
+    bookmark.attr(can.deparam(el.closest("form").serialize()));
+    this.saveBookmark(bookmark);
+  },
+  saveBookmark: function(bookmark) {
+    bookmark.save(this.clearForm.bind(this), this.signalError);
+  },
+  signalError: function() {
+    alert("The input is not valid.");
+  },
+  ".clear click": function (el, evt) {
+    evt.preventDefault();
+    this.clearForm();
+  }
 });
 
-getBookmark: function (el) {
-  return el.closest("li").data("bookmark");
-},
-".delete click": function(el, evt) {
-  this.getBookmark(el).destroy();
-},
+var App_base = can.Construct.extend({
+  init: function() {
+    Bookmark.findAll({}, function(bookmarks) {
+      var eventHub = new can.Observe({});
+      var options = {eventHub: eventHub, bookmarks: bookmarks};
+
+      new BookmarkListControl("#bookmark_list_container", options);
+      new BookmarkFormControl("#bookmark_form_container", options);
+    });
+  }
+});
+
 
